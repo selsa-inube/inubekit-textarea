60a03a49b10fc467d3a6e68819038bb5202ed35f
Andres Babativa <carlos3k11@gmail.com>
2024-02-06 15:09:50 -0500

    feat: remove unused dependencies and update Vite configuration

--- vite.config.ts
+++ vite.config.ts
@@ -11,6 +11,14 @@ export default defineConfig({
       formats: ["es"],
       fileName: (format) => `index.${format}.js`,
     },
+    rollupOptions: {
+      external: ["react", "styled-components"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
   },
   plugins: [react(), dts({ rollupTypes: true })],
 });
