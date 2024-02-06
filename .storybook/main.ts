f7f26708ba32af5922653a4d5de0f8b311038661
Andres Babativa <carlos3k11@gmail.com>
2024-02-06 15:06:29 -0500

    feat(Storybook): disable telemetry and update documentation settings

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -2,10 +2,12 @@ import type { StorybookConfig } from "@storybook/react-vite";
 
 const config: StorybookConfig = {
   stories: ["../src/**/*.mdx", "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"],
+  core: {
+    disableTelemetry: true,
+  },
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-onboarding",
     "@storybook/addon-interactions",
   ],
   framework: {
@@ -13,7 +15,8 @@ const config: StorybookConfig = {
     options: {},
   },
   docs: {
-    autodocs: "tag",
+    autodocs: true,
+    defaultName: "Documentation",
   },
 };
 export default config;
